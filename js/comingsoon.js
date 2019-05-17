907396669c2d0062f454e8f5cbe48d0c2842ae66
Samarth Gowda <samarthgowda@outlook.com>
2019-05-17 09:59:38 -0400

    First commit of website

---
+++ js/comingsoon.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $("#comingsoon").load("/pages/templates/comingsoon.html");        
+    });
---
+++ js/nav_footer.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#nav").load("/pages/templates/nav.html");        
+    });
+
+$(document).ready(function() {
+    $("#footer").load("/pages/templates/footer.html");        
+    });
